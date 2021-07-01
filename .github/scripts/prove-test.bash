b30d8d9415235a1dbd01bc8397c15614f4427112
Simon Reinhardt <simon.reinhardt@physik.uni-r.de>
2021-07-01 19:27:01 +0200

    try perl action

--- .github/scripts/prove-test.bash
+++ .github/scripts/prove-test.bash
@@ -1,5 +1,15 @@
-printenv
+
+# Prevent "Please tell me who you are" errors for certain DZIL configs
 git config --global user.name 'github-actions'
+
+# check perl version
+perl -V
+
+
+#
+# install gnuplot version specified by environment variable $gp_version
+#
+
 cd /tmp
 wget https://github.com/lab-measurement/Lab-Measurement-Homepage/raw/master/gnuplot-5.2.4.tar.gz
 tar -xf gnuplot-$gp_version.tar.gz
@@ -8,9 +18,10 @@ cd gnuplot-$gp_version
 make
 make install
 export PATH="$HOME/local/bin:$PATH"
-gnuplot --version
+gnuplot --version 
 # Sometimes Alien::Gnuplot does not find the installed gnuplot
 # (but problem not reproducible, see
 # https://travis-ci.org/lab-measurement/Lab-Measurement/jobs/378743671
 export GNUPLOT_BINARY=$HOME/local/bin/gnuplot
+
 
